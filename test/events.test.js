f26243a19d33db6d10fff48abff9bf05a9e92635
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-31 11:32:05 -0700

    expecting any string in get: today date

--- test/events.test.js
+++ test/events.test.js
@@ -43,24 +43,22 @@ describe('app routes', () => {
       });
   });
 
-  // it('get all events happening today with GET:today', () => {
-  //   console.log(today);
-  //   console.log(yesterday);
-  //   return request(app)
-  //     .get('/api/v1/events/today')
-  //     .then(res => {
-  //       res.body.forEach(event => {
-  //         expect(event).toEqual({
-  //           _id: expect.any(String),
-  //           startTime: expect.stringContaining({ yesterday, today }),
-  //           endTime: expect.any(String),
-  //           eventId: expect.any(String),
-  //           description: expect.any(String),
-  //           signUpUrl: expect.any(String),
-  //           title: expect.any(String)
-  //         });
-  //       });
-  //       expect(res.body).toHaveLength(5);
-  //     });
-  // });
+  it('get all events happening today with GET:today', () => {
+    return request(app)
+      .get('/api/v1/events/today')
+      .then(res => {
+        res.body.forEach(event => {
+          expect(event).toEqual({
+            _id: expect.any(String),
+            startTime: expect.any(String),
+            endTime: expect.any(String),
+            eventId: expect.any(String),
+            description: expect.any(String),
+            signUpUrl: expect.any(String),
+            title: expect.any(String)
+          });
+        });
+        expect(res.body).toHaveLength(5);
+      });
+  });
 });
