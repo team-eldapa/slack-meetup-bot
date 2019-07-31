9363102c9c30c678dfd2c7fa17b809382e22db9a
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-30 17:54:20 -0700

    other tests refactored and passing now

--- test/events.test.js
+++ test/events.test.js
@@ -1,14 +1,16 @@
 require('./datahelpers');
+const events = require('./seed-data');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  const event = events[0];
+
   it('gets all events with GET', () => {
     return request(app)
       .get('/api/v1/events')
       .then(res => {
-        console.log(res.body);
         res.body.forEach(event => {
           expect(event).toEqual({
             _id: expect.any(String),
@@ -23,38 +25,38 @@ describe('app routes', () => {
       });
   });
 
-  // it('gets an event by its Id with GET:id', () => {
-  //   const event = events[0];
-  //   return request(app)
-  //     .get(`/api/v1/events/${event._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: event._id.toString(),
-  //         startTime: event.startTime,
-  //         endTime: event.endTime,
-  //         eventId: event.eventId,
-  //         description: event.description,
-  //         signUpUrl: event.signUpUrl,
-  //         title: event.title
-  //       });
-  //     });
-  // });
+  it('gets an event by its Id with GET:id', () => {
+    return request(app)
+      .get(`/api/v1/events/${event._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: event._id.toString(),
+          startTime: event.startTime,
+          endTime: event.endTime,
+          eventId: event.eventId,
+          description: event.description,
+          signUpUrl: event.signUpUrl,
+          title: event.title
+        });
+      });
+  });
 
-  // it('get all events happening today with GET:today', () => {
-  //   return request(app)
-  //     .get('/api/v1/events/today')
-  //     .then(res => {
-  //       res.body.forEach(event => {
-  //         expect(event).toEqual({
-  //           _id: expect.any(String),
-  //           endTime: expect.any(String),
-  //           eventId: expect.any(String),
-  //           description: expect.any(String),
-  //           signUpUrl: expect.any(String),
-  //           title: expect.any(String)
-  //         });
-  //       });
-  //       expect(res.body).toHaveLength(5);
-  //     });
-  // });
+  it('get all events happening today with GET:today', () => {
+    return request(app)
+      .get('/api/v1/events/today')
+      .then(res => {
+        res.body.forEach(event => {
+          expect(event).toEqual({
+            _id: expect.any(String),
+            startTime: expect.any(String),
+            endTime: expect.any(String),
+            eventId: expect.any(String),
+            description: expect.any(String),
+            signUpUrl: expect.any(String),
+            title: expect.any(String)
+          });
+        });
+        expect(res.body).toHaveLength(5);
+      });
+  });
 });
