ef7b077889f1c32d2b8266c3062729c272f81f3c
Dan Hogan <hogan.daniel@hotmail.com>
2019-07-31 13:13:54 -0700

    Add bot commands

--- slackbot/bot.js
+++ slackbot/bot.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const SlackBot = require('slackbots');
 const token = process.env.SLACK_BOT;
+const request = require('superagent');
+// const app = require('../lib/app');
 
 const bot = new SlackBot({
   token,
@@ -13,46 +15,83 @@ bot.on('start', () => {
   };
   bot.postMessageToChannel(
     'general',
-    'Hello TEstiNG teSTINg',
+    'Hello, <!channel>! I\'m MeetupBot, your helper for finding PDX tech meetups.',
     params
   );
 });
 
 bot.on('message', data => {
-  if(!data.content) return;
-  const user = data.content.split(': @MeetupBot ')[0];
-  const message = data.content.split(': @MeetupBot ')[1];
-
-  handleMessage(user, message);
+  if(data.type !== 'message' || data.subtype === 'bot_message') return;
+  const user = data.user;
+  const message = data.text;
+  const channel = data.channel;
+  handleMessage(user, message, channel);
 });
 
-function handleMessage(user, message) {
-  if(message.includes(' help')) {
-    sendHelp(user);
+function handleMessage(user, message, channel) {
+  if(message.includes(' /help')) {
+    sendHelp(user, channel);
+  } else if(message.includes(' /all-events')) {
+    sendAllEvents(user, channel);
   } else {
-    sendUnrecognized(user);
+    sendUnrecognized(user, channel);
   }
 }
 
-const sendHelp = user => {
+const sendAllEvents = (user, channel) => {
+  const formattedEvents = {
+    text: ''
+  };
+  return request
+    .get('http://localhost:7890/api/v1/events')
+    .then(res => res.body)
+    .then(events => {
+      events.forEach(event => {
+        const options = {
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: true
+        };
+
+        const startTime = new Date(event.startTime);
+        const endTime = new Date(event.endTime);
+
+        formattedEvents.text += `*${event.slackId}*: \`${event.title}\`
+        _${startTime.toLocaleDateString()}: ${startTime.toLocaleString('en-US', options)} - ${endTime.toLocaleString('en-US', options)}_ 
+        \n`;
+
+      });
+    })
+    .then(() => {
+      bot.postMessage(
+        channel,
+        formattedEvents.text
+      );
+      console.log('sent');
+    });
+
+};
+
+const sendHelp = (user, channel) => {
   const params = {
     icon_emoji: ':question:'
   };
-  bot.postMessageToChannel(
-    'general',
-    `Hey ${user}! Here are the things you can ask me to do:
-      - '@meetupbot weekly' for weekly events.....`,
+  bot.postMessage(
+    channel,
+    `Hey, <@${user}>! \n *Here are the things you can ask me to do:*
+      - \`@meetupbot /all-events\` to get all upcoming events.
+      - \`@meetupbot /weekly\` for weekly events.`,
     params
   );
 };
 
-const sendUnrecognized = user => {
+const sendUnrecognized = (user, channel) => {
   const params = {
     icon_emoji: ':interrobang:'
   };
-  bot.postMessageToChannel(
-    'general',
-    `Uh oh ${user}, I didn't recognize that command. Try using '@meetupbot help'.`,
+  bot.postMessage(
+    channel,
+    `Uh oh <@${user}>, I didn't recognize that command. Try using '@meetupbot help'.`,
     params
   );
 };
