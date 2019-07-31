a0e96033f6f57075bac52405f33526b7a0cbe8ca
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-31 14:09:20 -0700

    created user model, user routes file, test, etc

--- test/datahelpers.js
+++ test/datahelpers.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const seedData = require('./seed-data');
 const Event = require('../lib/models/Event');
+const User = require('../lib/models/User');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
@@ -9,6 +10,15 @@ const url = process.env.MONGODB_TEST_URI || process.env.MONGODB_URI;
 function seed(seedData) {
   Event
     .create(seedData);
+
+  User
+    .create({
+      name: 'George Ludocrouix',
+      signedUpEvents: ['1250475851', '1250475745', '1250475879']
+    }, {
+      name: 'Mark McFrank',
+      signedUpEvents: ['1250475901', '1250475745', '1250475929', '1250475845']
+    });
 }
 
 beforeAll(async() => {
---
+++ test/users.test.js
@@ -0,0 +1,12 @@
+require('./datahelpers');
+// const events = require('./seed-data');
+
+// const request = require('supertest');
+// const app = require('../lib/app');
+
+describe('users routes tests', () => {
+  it('is a passing test', () => {
+    const something = 'hi';
+    expect(something).toEqual('hi');
+  });
+});
